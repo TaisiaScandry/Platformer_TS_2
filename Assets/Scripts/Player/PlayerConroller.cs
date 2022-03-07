d5e6f696c06fb44072acbf5cc605cc9867aa5ba0
vntillow <97822445+vntillow@users.noreply.github.com>
2022-03-07 16:31:37 +1000

    initial commit

---
+++ Assets/Scripts/Player/PlayerConroller.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConroller : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    bool isGrounded;
+    public float velocity;
+    public float jumpHeight;
+    public Transform groundCheck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") == 0 && isGrounded)
+        {
+            animator.SetInteger("State", 1);
+        }
+        flip();
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpHeight, ForceMode2D.Impulse);
+        }
+        GroundCheck();
+    }
+
+    // Ratation of player
+    void flip()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * velocity, rb.velocity.y);
+    }
+
+    void GroundCheck()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, 0.2f);
+        isGrounded = colliders.Length > 1;
+    }
+}
